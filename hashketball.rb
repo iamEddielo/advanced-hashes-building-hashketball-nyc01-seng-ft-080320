932cee6c7fdf8cd4063270c32ec85e2e6213e63c
Eddie Lopez <iameddielopez@gmail.com>
2020-07-24 21:43:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,15 +1,37 @@
 # Write your code here!
 def game_hash
   game_hash = {
-    :home =>{
+      :home =>{
       :team_name =>"Brooklyn Nets",
       :colors => [ "Black","White"],
-      :players =>[{:player_name =>"",:number => i,:shoe => i,:points => i, :rebounds => i,:assists => i, :steals => i,:blocks => i, slam_dunks =>i}]
+      :players =>[
+        { :player_name =>"",
+          :number => i,
+          :shoe => i,
+          :points => i, 
+          :rebounds => i,
+          :assists => i, 
+          :steals => i,
+          :blocks => i, 
+          :slam_dunks =>i
+          }
+          ]
       },
     :away =>{
         :team_name => "Charlotte Hornets",
         :colors => ["Turquoise","Purple"],
-        :players =>[ {:player_name =>"",:number => i,:shoe => i,:points => i, :rebounds => i,:assists => i, :steals => i,:blocks => i, slam_dunks =>i}]
+        :players =>[ 
+        {:player_name =>"",
+          :number => i,
+          :shoe => i,
+          :points => i, 
+          :rebounds => i,
+          :assists => i, 
+          :steals => i,
+          :blocks => i, 
+          :slam_dunks =>i
+          }
+        ]
       }
       }
 end
