b1cb16433d60c44aac16b37b993ca56aa31578dc
Eddie Lopez <iameddielopez@gmail.com>
2020-07-24 21:29:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
       },
     :away => {
         :team_name => "Charlotte Hornets",
-        :colors => ["Turquoise" ,],
+        :colors => ["Turquoise","Purple"],
         :players =>[ ]
       }
 end
