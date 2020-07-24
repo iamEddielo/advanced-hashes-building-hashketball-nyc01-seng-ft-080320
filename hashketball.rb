82c8e2f70e65ec712cd158dc7a149c95740715de
Eddie Lopez <iameddielopez@gmail.com>
2020-07-24 21:45:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,15 @@ def game_hash
       :team_name =>"Brooklyn Nets",
       :colors => [ "Black","White"],
       :players =>[
-        { :player_name =>"",
-          :number => i,
-          :shoe => i,
-          :points => i, 
-          :rebounds => i,
-          :assists => i, 
-          :steals => i,
-          :blocks => i, 
-          :slam_dunks =>i
+        { :player_name =>"Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22, 
+          :rebounds => 12,
+          :assists => 12, 
+          :steals => 3,
+          :blocks => 1, 
+          :slam_dunks =>1
           }
           ]
       },
